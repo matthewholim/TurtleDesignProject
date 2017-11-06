882634d060cf2a8232bb441659c800718a74ce35
matthewholim <33330162+matthewholim@users.noreply.github.com>
2017-11-06 15:16:32 -0500

    Add files via upload

---
+++ my des.py
@@ -0,0 +1,258 @@
+
+import turtle
+bob = turtle.Turtle()
+from functions import*
+turtle.bgcolor("black")
+turtle.colormode(255)
+#bob.width(2)
+bob.speed(100)
+#c = (255,255,255)
+#bob.color(c)
+
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+jump(bob,200,-200)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+        
+jump(bob,-200,-200)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+        
+jump(bob,200,200)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+
+jump(bob,-200,200)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+        
+jump(bob,0,380)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+
+jump(bob,-400,0)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+
+jump(bob,400,0)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+
+jump(bob,0,-360)
+
+for times in range(20):
+    for c in ["red","yellow","blue","green"]:
+        bob.color(c)
+        bob.circle(times+20)
+        bob.right(90)
+        bob.forward(25)   
+        bob.left(90)
+        bob.forward(100)  
+        bob.left(90)
+        bob.forward(50)
+        bob.left(90)
+        bob.forward(100)
+        bob.left(90)
+        bob.forward(50)
+
+jump(bob,400,-380)
+
+for times in range(20):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.circle(50)
+        bob.right(10)
+
+jump(bob,-400,380)
+
+for times in range(20):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.circle(50)
+        bob.right(10)
+
+jump(bob,400,380)
+
+for times in range(20):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.circle(50)
+        bob.right(10)
+        
+jump(bob,-400,-380)
+
+for times in range(20):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.circle(50)
+        bob.right(10)
+
+jump(bob,570,0)
+
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+        
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+        
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+
+jump(bob,-690,0)
+
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+        
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+        
+for times in range(50):
+    for c in ["yellow","blue","red","green"]:
+        bob.color(c)
+        bob.forward(5)
+        bob.right(times)
+    
